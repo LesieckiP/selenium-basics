2ada969f062450693ad36eecb3fa5e7457f5b4e0
Piotr Lesiecki <piotr.lesiecki@ingbank.pl>
2016-02-22 13:42:35 +0100

    Selenium simple example

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,12 +1,18 @@
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
 
 public class FirstTest {
 
+	WebDriver driver = new FirefoxDriver();
+
 	@Test
 	public void firstTest(){
-		assertTrue(false);
+		driver.get("http://papryqarz.org");
+
+		assertEquals("Nieprawidłowy tytuł strony PapryQarz", "papryQArz - We test with taste", driver.getTitle());
 	}
 
 }
